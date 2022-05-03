2d965f8863d84b6b42bb5b8e7c9a0c9fd9587029
Hannah Kim <devhkim13@gmail.com>
2022-05-03 10:48:49 -0700

    Add computerPlay function to determine computer's play

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/**
+ * Randomly returns a string value of either "Rock," "Paper," or
+ * "Scissors" to represent the computer's play.
+ */
+function computerPlay() {
+    // Generate a random number between 1-3
+    const randomNum = Math.floor(Math.random() * (3)) + 1;
+
+    // Output "Rock," "Paper," or "Scissors" based on number
+    if (randomNum === 1) {
+        console.log("Rock");
+    } else if (randomNum === 2) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
+}
